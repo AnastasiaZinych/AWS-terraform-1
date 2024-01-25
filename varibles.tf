9cfc68efb67fa9aede9dfeddc93bd777681ee59f
AnastasiiaZinych <zinychnastya@gmail.com>
2024-01-25 05:31:49 +0000

    add efs.tf

--- varibles.tf
+++ varibles.tf
@@ -9,3 +9,38 @@ variable "sec_group_name" {
   type        = string
   default     = "my_sg"
 }
+
+variable "public_subnet1" {
+  description = "please provide a subnet information"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet2" {
+  description = "please provide a subnet information"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "public_subnet3" {
+  description = "please provide a subnet information"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+variable "private_subnet1" {
+  description = "please provide a subnet information"
+  type        = string
+  default     = "10.0.4.0/24"
+}
+
+variable "private_subnet2" {
+  description = "please provide a subnet information"
+  type        = string
+  default     = "10.0.5.0/24"
+}
+
+variable "private_subnet3" {
+  description = "please provide a subnet information"
+  type        = string
+  default     = "10.0.6.0/24"
+}
