e46d54954e360dfe28c3000f05299505e9c46961
AnastasiiaZinych <zinychnastya@gmail.com>
2024-01-25 04:54:53 +0000

    add alb

--- varibles.tf
+++ varibles.tf
@@ -3,3 +3,9 @@ variable "path_to_public_key" {
   type        = string
   default     = "/home/ec2-user/.ssh/id_rsa.pub"
 }
+
+variable "sec_group_name" {
+  description = "please provide a sec_group_name information"
+  type        = string
+  default     = "my_sg"
+}
