dda554da57b6ac78af11999644113adc389b5e24
AnastasiiaZinych <zinychnastya@gmail.com>
2024-01-27 23:10:55 +0000

    update instalation

--- installation.sh
+++ installation.sh
@@ -12,3 +12,18 @@ sudo yum -y install packer
 
 
 # bash installation.sh
+ provisioner "remote-exec" {
+  inline = [
+    "sudo yum update -y",
+    "sudo yum install -y httpd php php-mysqlnd",
+    "sudo systemctl start httpd",
+    "sudo systemctl enable httpd",
+    "sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2",
+    "cd /var/www/html",
+    "sudo wget https://wordpress.org/latest.tar.gz",
+    "sudo tar -xzf latest.tar.gz",
+    "sudo cp -R wordpress/* /var/www/html/",
+    "sudo chown -R apache:apache /var/www/html/",
+    "sudo systemctl restart httpd"
+   ]
+  }
--- launchtemplate.tf
+++ launchtemplate.tf
@@ -1,11 +1,13 @@
 # Launch template
 resource "aws_launch_template" "mlt" {
-  name          =  "mlt"
-  image_id      =  "ami-0587a19e199d10a2e"
+  name = "mlt"
+  image_id      =  aws_ami_from_instance.wordpress.id
   instance_type =  "t2.micro"
   key_name      =  aws_key_pair.project.id
+
   network_interfaces {
     associate_public_ip_address = true
-    security_groups             = [aws_security_group.my_sg.id]
+    security_groups             = [aws_security_group.my_sg.id]  
   }
+  user_data = filebase64("${path.module}/installation.sh")a
 }
